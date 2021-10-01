7769c37c0ba8e592acf84265bee43a11dcd9e683
Dave Clark <dave.clark@capgemini.com>
2021-10-01 10:22:23 +0100

    sample data retrieval with proxy api, react app

---
+++ src/Sessions.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = styled.h1`
+  color: blue;
+  background-color: red;
+`;
+
+class Sessions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("_odata/Sessions")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({
+            isLoaded: true,
+            items: result.value,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map((item) => (
+            <li key={item.dc_sessionid}>
+              {item.dc_account.Name}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+export default Sessions;
